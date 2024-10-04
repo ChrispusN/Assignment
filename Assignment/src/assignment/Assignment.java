1f38ca5bdde1729828bd6b77f1e2117273febaaa
ChrispusN <niwamanyachrispus35@gmail.com>
2024-10-04 16:49:49 +0300

    Add files via upload

---
+++ Assignment/src/assignment/Assignment.java
@@ -0,0 +1,20 @@
+package assignment;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+//	using a for loop to print numbers from 1 to 5
+System.out.println("Using for loop");
+	for (int i = 1; i <= 5; i++) {	
+		System.out.println(i);	
+	}
+	//Using a while loop to print numbers from 1 to 5
+	System.out.println("Using while loop");
+	int j = 1;
+	while (j<= 5) {
+		System.out.println(j);
+		j++;
+	}
+	}
+	
+}
---
+++ Assignment/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author CHRIS
+ *
+ */
+module Assignment {
+}
